ce643fcd0c64dcd046b834b9c077009117282680
belatent <avalonzdf@gmail.com>
2016-05-03 11:55:40 +0800

    Refactor the file structure. Create the game structure and add 2 plugins: iTween and NGUI. The game now can divide the map into blocks and the game object now can moving by following the blocks.

---
+++ game/Assets/script/Camear.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camear : MonoBehaviour {
+    Vector3[] Pos = new Vector3[2];
+    int point = 0;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        coordinate();
+    }
+
+    void move()
+    {
+        if (point < 2)
+        {
+            iTween.MoveTo(player, iTween.Hash("position", Pos[point], "speed", 50f, "caseType", "linear", "Oncomplete", "move", "oncompletetarget", gameObject));
+            point++;
+
+        }
+
+    }
+
+    void coordinate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
+
+            Pos[0] = new Vector3(hit.collider.transform.position.x, player.transform.position.y, 0);
+            Pos[1] = new Vector3(hit.collider.transform.position.x, hit.collider.transform.position.y, 0);
+
+            point = 0;
+            move();
+            //print(hit.point.x +"   "+ hit.point.y);
+            print(Pos[0]);
+
+        }
+    }
+}
---
+++ game/Assets/script/CreateBlocks.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBlocks : MonoBehaviour {
+    BoxCollider backGround;
+    float bkWidth;
+    float bkLength;
+
+    BoxCollider block;
+    float blockWidth;
+    float blockLength;
+
+    int length;
+    int width;
+
+    public GameObject singleBlock;
+	// Use this for initialization
+	void Start () {
+        setUp();
+        print("block nums\t"+length+"\t"+width);
+        print("block size\t\t" + blockLength + "\t" + blockWidth);
+        print("background size\t" + bkLength + "\t" + bkWidth);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void getInfo()
+    {
+        //get background size
+        backGround = this.gameObject.GetComponent<BoxCollider>();
+        bkLength = backGround.size.x;
+        bkWidth = backGround.size.y;
+        //get block size
+        block = singleBlock.GetComponent<BoxCollider>();
+        blockLength = block.size.x;
+        blockWidth = block.size.y;
+    }
+
+    void setUpBlocksLW()
+    {
+        length = (int)(bkLength / blockLength);
+        width = (int)(bkWidth / blockWidth);
+    }
+
+    void createBlocks()
+    {
+        //print(singleBlock.GetComponents<BoxCollider>().Length);
+        //foreach(BoxCollider collider in singleBlock.GetComponents<BoxCollider>())
+        //    Destroy(collider);
+
+        //singleBlock.AddComponent<BoxCollider>();
+        //singleBlock.GetComponent<BoxCollider>().size.Set(blockLength,blockWidth,(float)0.2);
+
+        for(int i = -length/2; i < length/2; i++)
+        {
+            for(int j = -width/2; j < width/2; j++)
+            {
+                singleBlock = (GameObject)Instantiate(singleBlock, new Vector3(i*blockLength + (float)1.28/2, j* blockWidth + (float)1.28/2, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    void setUp()
+    {
+        getInfo();
+        setUpBlocksLW();
+        createBlocks();
+    }
+
+}
