d82f5bdc60e3b4b822f700d84a2f1f4ea4c8a943
belatent <avalonzdf@gmail.com>
2016-09-05 15:56:28 -0500

    Revert "Finish database functionalities"
    This reverts commit 000e71807670d03b215fbc280d88292341640659.

--- project/Assets/script/Character.cs
+++ project/Assets/script/Character.cs
@@ -1,78 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.IO;
-using Mono.Data.Sqlite;
+
 public class Character : MonoBehaviour
 {
-    /// SQLite数据库辅助类
-    public static SQLiteHelper sql = new SQLiteHelper("data source=Game.db");
-    public static SqliteDataReader reader;
+    public class attribute {
+        int level;
+        public int movingRange { get; set; }
+        int atk;
+        int def;
+        int speed;
+        int magicAtk;
+        int magicDef;
+        float avo;
+        int luck;
 
-    public Attribute attr { get; set; }
+        public attribute()
+        {
+            movingRange = 4;
+        }
+    }
+
+    public attribute attr { get; set; }
 
     // Use this for initialization
     void Start()
     {
-        attr = new Attribute(sql, reader);
+        attr = new attribute();
     }
-
-    public class Attribute
-    {
-        public int movingRange { get; set; }
-        private string name;
-        private string type;
-        private int level;
-        private float hp;
-        private float mp;
-        private float ap;
-        private float atk;
-        private float def;
-        private float magicAtk;
-        private float magicDef;
-        private float speed;
-        // private float avo;
-        private float luck;
-
-        public Attribute(SQLiteHelper sql, SqliteDataReader reader)
-        {
-            movingRange = 4;
-            loadInfo(sql, reader, "Alice");
-            Debug.Log(toString());
-        }
-
-
-        private void loadInfo(SQLiteHelper sql, SqliteDataReader reader, string name)
-        {
-            reader = sql.ReadFullTable("Characters");
-            while (reader.Read())
-            {
-                if (reader.GetString(reader.GetOrdinal("Name")).Equals(name))
-                {
-                    this.name = reader.GetString(reader.GetOrdinal("Name"));
-                    this.type = reader.GetString(reader.GetOrdinal("Type"));
-                    this.level = reader.GetInt32(reader.GetOrdinal("Level"));
-                    this.hp = reader.GetFloat(reader.GetOrdinal("HP"));
-                    this.mp = reader.GetFloat(reader.GetOrdinal("MP"));
-                    this.ap = reader.GetFloat(reader.GetOrdinal("AP"));
-                    this.atk = reader.GetFloat(reader.GetOrdinal("ATK"));
-                    this.def = reader.GetFloat(reader.GetOrdinal("DEF"));
-                    this.magicAtk = reader.GetFloat(reader.GetOrdinal("MATK"));
-                    this.magicDef = reader.GetFloat(reader.GetOrdinal("MDEF"));
-                    this.speed = reader.GetFloat(reader.GetOrdinal("SPD"));
-                    this.luck = reader.GetFloat(reader.GetOrdinal("LUK"));
-                }
-            }
-        }
-
-        public string toString()
-        {
-            return "Name: " + name + "\nType: " + type + "\nLevel: " + level + "\nHP: " + hp + "\nMP: " + mp + "\nAP: " + ap +
-                   "\nATK: " + atk + "\nDEF" + def + "\nMATK: " + magicAtk + "\nMDEF: " + magicDef + "\nSPD: " + speed + "\nLUK: " + luck;
-        }
-
-
-
-    } // End class Attribute
 
     // Update is called once per frame
     void Update()
